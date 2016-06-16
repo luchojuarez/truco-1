e82c23f3d611583f6a05dd44dd2b23a8476e2db4
Rodrigo Grazini <grazini.rodrigo@gmail.com>
2016-06-16 00:53:12 -0300

    Modificar game.js test, agregar un test mas
    Ya no es necesario crear el juego con un jugador como mano, por defecto
    es player1
    
    nuevo test donde se juegan todos empates y los puntos los gana la mano
    tambien se canto truco

--- test/game.js
+++ test/game.js
@@ -21,7 +21,7 @@ describe('Game#play', function(){
   var game;
 
   beforeEach(function(){
-    game = new Game({ currentHand: 'player1' });
+    game = new Game();
     game.player1 = new Player({ nickname: 'J' });
     game.player2 = new Player({ nickname: 'X' });
     game.newRound();
@@ -92,12 +92,38 @@ describe('Game#play', function(){
    it('plays some cards then player2 chants truco and player 1 declines it, increase 1 in the score of player 2 ', function() {
 	var cardsp1 = game.player1.cards;
 	var cardsp2 = game.player2.cards;
-	var oldscore = game.score;
+	var oldscore = game.score[1];
 	game.play('player1', 'playCard', cardsp1[0]); //1 espada
 	game.play('player2','playCard', cardsp2[1]); //4 basto
 	game.play('player1','playCard', cardsp1[1]); //3 oro
 	game.play('player2','playCard', cardsp2[0]); // 7 oro
 	game.play('player2','truco');
+	game.play('player1','no_quiero');
 	expect(game.score[1]).to.be.equal(oldscore+1);
    });
+
+   it('plays a round with all ties, player 1(the hand) wons, also truco is chanted', function() {
+    game.player1.setCards([
+        new Card(4, 'espada'),
+        new Card(5, 'oro'),
+        new Card(6, 'espada')
+    ]);
+
+    game.player2.setCards([
+        new Card(4, 'oro'),
+        new Card(5, 'basto'),
+        new Card(6, 'basto')
+    ]);
+ 	var cardsp1 = game.player1.cards;
+	var cardsp2 = game.player2.cards;
+	game.play('player1','playCard', cardsp1[0]);
+	game.play('player2','playCard', cardsp2[0]);
+	game.play('player1','playCard', cardsp1[1]);
+	game.play('player2','truco');
+	game.play('player1','quiero');
+	game.play('player2','playCard', cardsp2[1]); 
+	game.play('player1','playCard', cardsp1[2]); 
+	game.play('player2','playCard', cardsp2[2]);
+	expect(game.score[0]).to.be.equal(2);
+    });
 });
