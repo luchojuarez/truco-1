fbc3a0783052f7afde6aae3888de51a444f9c9e6
Luciano Juarez <ljuarez@dc.exa.unrc.edu.ar>
2016-06-19 04:23:11 -0300

    new view 'login guest'

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,12 @@ var User = require('../models/user');
 var router = express.Router();
 
 var Game = require("../models/game").game;
+var Round = require("../models/round").round;
+var guest;
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  var game = new Game();
-  res.render('index', { user : req.user , user2 : req.user2});
+  res.render('index', { user : req.user , guest : guest});
 });
 
 router.get('/register', function(req, res) {
@@ -28,24 +29,34 @@ router.post('/register', function(req, res) {
 });
 
 router.get('/login', function(req, res) {
-    res.render('login', { user : req.player});
+    res.render('login', { user : req.user});
 });
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
     res.redirect('/');
 });
 
+router.get('/loginGuest',function (req,res) {
+    res.render('loginGuest')
+})
+
+router.post('/loginGuest',function (req,res) {
+    guest=req.body.username;
+    //console.log(req.body.username);
+    res.redirect('/')
+})
+
 router.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
 });
 
-router.get('/ping', function(req, res){
-    res.status(200).send("pong!");
-});
 
 router.get('/play',function (req,res) {
-    res.render('play')
+    res.render('play',{user:req.user,guest:guest})
+})
+router.post('/play',function (req,res) {
+
 })
 
 module.exports = router;
