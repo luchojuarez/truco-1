f705853261a1e651a356c8bd4888fbaa6705fe1c
Rodrigo Grazini <grazini.rodrigo@gmail.com>
2016-10-29 18:13:26 -0300

    Add on connection socket in index

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,21 @@ var passport = require('passport');
 var User = require('../models/user');
 var router = express.Router();
 
-var Game = require("../models/game").game;
-var Player = require("../models/player").player;
-var Round = require("../models/round").round;
-var Card = require("../models/card").card;
-
 
 
 
 /* GET home page. */
-router.get('/', function (req, res) {
-  res.render('index', { user : req.user});
-});
-
+router.get('/', function(req, res) {
+        res.io.on('connection', function(socket) {
+            if (req.user)console.log("user connected as",req.user.username)
+            socket.on('disconnect', function() {
+                console.log('user disconnected');
+            });    
+        });
+        res.render('index', {
+                    user: req.user
+                });    
+        });
 
 
 
@@ -23,49 +25,5 @@ router.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
 });
-function getAllPlayers(callback) {
-    User.find()
-    .exec(function (err, players) {
-        if (err){
-            callback(err,undefined);
-            console.error(err);
-        }
-        callback(err,players)
-    })
-}
 
-
-function parseCard (carta) {
-    var number = parseInt(carta.split("",2)[0]+carta.split("",2)[1]);
-	var suit = carta.split("",7);
-	if (suit[5]===' ') {
-		suit=suit[6];
-	}else{
-		suit=suit[5]
-	}
-	switch (suit){
-		case 'e':
-			suit = 'espada';
-			break;
-		case 'b':
-			suit = 'basto';
-			break;
-		case 'c':
-			suit = 'copa'
-			break;
-		case 'o':
-			suit='oro'
-			break;
-	}
-    return new Card(number,suit);
-}
-
-function restoreValues (game) {
-            game.currentRound.fsm = actualValues.FSM;
-            game.currentRound.board = actualValues.board;
-            game.player1 = actualValues.player1;
-            game.player2 = actualValues.player2;
-            game.score = actualValues.score;
-            game.currentRound.game = game;
-}
 module.exports = router;
